760e55099cc1074d56bdcf40efbb480fa585d2a8
Yves-Marie Saout <ymsaout42@gmail.com>
2023-05-24 01:54:29 +0200

    Networks setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,21 @@ require("@nomicfoundation/hardhat-toolbox");
 // testing the frontend.
 require("./tasks/faucet");
 
+require('dotenv').config();
+
+const { INFURA_PROJECT_ID, PRIVATE_KEY } = process.env;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [PRIVATE_KEY]
+    },
+    mumbai: {
+      url: `https://rpc-mumbai.maticvigil.com`,
+      accounts: [PRIVATE_KEY],
+    }
+  }
 };
